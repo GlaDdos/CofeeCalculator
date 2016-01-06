e1a90ca44bfc065d2fb388bb0ff51d368185fbd8
kamil_kawa <kamil.kawa@gmail.com>
2016-01-06 18:26:09 +0100

    Changes - working maths

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,27 +5,84 @@
  */
 var resultTable = [];
 var currentValue = [0];
+var evalValue = "";
 
-$(document).ready(function(){
-    console.log("hey it's a me mario!");
-    
-    $("button").on("click", function(){
-       if($(this).val() < 10){
-           currentValue.push(($(this).val()));
-       }
-       else if($(this).val() < 15){
-           console.log($(this).val());
-           resultTable.push(parseFloat(currentValue.join('')));
-           resultTable.push($(this).val());
-           currentValue = [0];
-       }
-           
-       else{
-           for(var i = 1; i < resultTable.length; i++){
-               
-           }
-       }
-       console.log(resultTable);
+
+$(document).ready(function () {
+    $("button").on("click", function () {
+        if ($(this).val() === 'C'){
+            currentValue = [0];
+            return 0;
+        }
+        
+        if ($(this).val() === 'AC'){
+            resultTable = [0];
+            evalValue = "";
+            currentValue = [0];
+            return 0;
+        }
+        
+        if ($(this).val() === 'BS'){
+            if(currentValue.length > 0){
+                currentValue.pop();
+            }
+            else{
+                currentValue = [0];
+            }
+            return 0;
+        }
+        
+        
+        if ($(this).val() < 10) {
+            currentValue.push(($(this).val()));
+            return 0;
+        } 
+        
+        if (isNaN($(this).val())) {
+            if ($(this).val() === '.') {
+                currentValue.push(($(this).val()));
+                
+            } else if ($(this).val() === 'EV'){
+                
+                if(currentValue.length > 0){
+                    resultTable.push(parseFloat(currentValue.join('')));
+                }
+                
+                if(isNaN(resultTable[resultTable.length - 1])){
+                    console.log("Syntax Error!");
+                    return 0;
+                }
+                
+                for (var i = 0; i < resultTable.length; i++) {
+                    evalValue += resultTable[i];
+                    console.log(evalValue);
+                }
+                
+                resultTable = [];
+                resultTable[0] = eval(evalValue);
+                evalValue = "";
+                currentValue = [];
+                
+            } else {
+                 
+                if (currentValue.length === 0 && isNaN(resultTable[resultTable.length - 1])) {
+                    resultTable.pop();
+                    resultTable.push($(this).val());
+                } else {
+                    if(currentValue.length > 0 && isNaN(resultTable[resultTable.length - 1])){
+                        resultTable.push(parseFloat(currentValue.join('')));
+                    } else if(currentValue.length > 0){
+                        resultTable = [];
+                        resultTable.push(parseFloat(currentValue.join('')));
+                    }
+                    resultTable.push($(this).val());
+                    currentValue = [];
+                }
+                              
+            }
+
+        } 
+        console.log(resultTable);
     });
 });
 
