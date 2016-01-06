98e99ef188080269e3f9a9bf36f22b3b020559eb
kamil_kawa <kamil.kawa@gmail.com>
2016-01-06 22:41:10 +0100

    Visual upgrade

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -7,11 +7,24 @@ var resultTable = [];
 var currentValue = [0];
 var evalValue = "";
 
+function updateView(){
+    
+    var strHTML = "";
+    if(arguments.length === 1){
+        strHTML = "<h3> " + parseFloat(arguments[0].join('')) + "</h3>";
+    }else if(arguments.length === 2){
+        strHTML = "<h3> " + parseFloat(arguments[0].join('')) +" "+ "</h3>";
+    }
+    
+    $(".result-container").html(strHTML);
+}
+
 
 $(document).ready(function () {
     $("button").on("click", function () {
         if ($(this).val() === 'C'){
-            currentValue = [0];
+            currentValue = [];
+            updateView([0]);
             return 0;
         }
         
@@ -19,28 +32,35 @@ $(document).ready(function () {
             resultTable = [0];
             evalValue = "";
             currentValue = [0];
+            updateView(currentValue);
             return 0;
         }
         
         if ($(this).val() === 'BS'){
-            if(currentValue.length > 0){
+            if(currentValue.length > 1){
                 currentValue.pop();
+                updateView(currentValue);
+                return 0;
             }
             else{
                 currentValue = [0];
+                //updateView(currentValue);
             }
+            updateView(currentValue);
             return 0;
         }
         
         
         if ($(this).val() < 10) {
             currentValue.push(($(this).val()));
+            updateView(currentValue);
             return 0;
         } 
         
         if (isNaN($(this).val())) {
             if ($(this).val() === '.') {
                 currentValue.push(($(this).val()));
+                updateView(currentValue);
                 
             } else if ($(this).val() === 'EV'){
                 
@@ -49,10 +69,12 @@ $(document).ready(function () {
                 }
                 
                 if(isNaN(resultTable[resultTable.length - 1])){
+                    resultTable.pop();
                     console.log("Syntax Error!");
                     return 0;
                 }
                 
+               
                 for (var i = 0; i < resultTable.length; i++) {
                     evalValue += resultTable[i];
                     console.log(evalValue);
@@ -62,6 +84,7 @@ $(document).ready(function () {
                 resultTable[0] = eval(evalValue);
                 evalValue = "";
                 currentValue = [];
+                updateView(resultTable);
                 
             } else {
                  
